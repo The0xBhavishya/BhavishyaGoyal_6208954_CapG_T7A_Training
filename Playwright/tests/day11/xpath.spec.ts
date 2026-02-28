308f08fb25e52d3c86a0becede8742f8a65bd600
bhaav <bhaav@gmail.com>
2026-02-28 10:42:39 +0530

    Updated Structure of stuff

--- Playwright/tests/day11/xpath.spec.ts
+++ Playwright/tests/day11/xpath.spec.ts
@@ -1,8 +1,17 @@
 import{test} from "@playwright/test"
 
 
-test("playwight",async(page)=>{
+test("login using xpath", async ({ page }) => {
+
+  await page.goto('https://techbeamers.com/practice-test-login/');
+   await page.locator('//input[@id="username"]').fill('student');
+  await page.locator('//input[@id="password"]').fill('Password123');
+  await page.locator('//button[@id="submit"]').click();
+
+});
+
+// test("playwight",async(page)=>{
 
 
 
-})
+// })
