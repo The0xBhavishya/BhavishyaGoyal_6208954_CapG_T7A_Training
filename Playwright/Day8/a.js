385e1d6b1ba6063f6f66b96aee00856225511afd
bhaav <bhaav@gmail.com>
2026-03-16 15:25:58 +0530

    daily

--- Playwright/Day8/a.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var math_1 = require("./math");
-var n1 = 12;
-var n2 = 2;
-var sum = (0, math_1.add)(n1, n2);
-console.log(sum);
-var math_2 = require("./math");
-console.log(math_2.a);
--- Playwright/tests/day19/Day19datajson.spec.ts
+++ Playwright/tests/day19/Day19datajson.spec.ts
@@ -1,40 +1,36 @@
-import { test } from "@playwright/test";
-import fs from "fs";
-import path from "path";
-
-//path.join(__dirname, "../testdata/data.json"); --> path for json file 
-let datafile  = fs.readFileSync(path.join(__dirname, "../testdata/data2.json"),"utf-8");
-let data = JSON.parse(datafile);
+// import { test } from "@playwright/test";
+// // Import JSON data directly
+// import data from "../testdata/data2.json";
 
 
-// test("json data", async({page}) => {
-//     //console.log(data.greet);
+// // test("json data", async({page}) => {
+// //     //console.log(data.greet);
 
-//     data.forEach(d=>{
-//         console.log(d.greet);
-//     })
+// //     data.forEach(d=>{
+// //         console.log(d.greet);
+// //     })
     
+// // })
+
+// test("for application", async({page}) => {
+//     //single object code
+//     // await page.goto(data.url);
+//     // await page.getByLabel("Username").fill(data.username);
+//     // await page.getByLabel("Password").fill(data.password);
+
+//     //array of objects code
+//     //we can also define array inside as valid,invalid array to define array of objects as valid or invalid
+    
+//     for(let d of data){
+//         let url = d.url;
+//         let username = d.username;
+//         let password = d.password;
+
+//         await page.goto(url);
+//         console.log(await page.title());
+//         await page.getByLabel("Username").fill(username);
+//         await page.getByLabel("Password").fill(password);
+//     }
+
+
 // })
-
-test("for application", async({page}) => {
-    //single object code
-    // await page.goto(data.url);
-    // await page.getByLabel("Username").fill(data.username);
-    // await page.getByLabel("Password").fill(data.password);
-
-    //array of objects code
-    //we can also define array inside as valid,invalid array to define array of objects as valid or invalid
-    
-    for(let d of data){
-        let url = d.url;
-        let username = d.username;
-        let password = d.password;
-
-        await page.goto(url);
-        console.log(await page.title());
-        await page.getByLabel("Username").fill(username);
-        await page.getByLabel("Password").fill(password);
-    }
-
-
-})
